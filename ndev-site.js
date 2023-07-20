dd3e0420a13373340dc1416f78f56deeb25a8326
nouhi <wolfgraphics.business@gmail.com>
2023-07-20 21:37:51 +0200

    centralized

---
+++ ndev-site.js
@@ -0,0 +1,11 @@
+// ░█▄─░█ ░█▀▀▄ ░█▀▀▀ ░█──░█ 　 ░█▀▀▀█ ▀█▀ ▀▀█▀▀ ░█▀▀▀ 　 ───░█ ░█▀▀▀█ 
+// ░█░█░█ ░█─░█ ░█▀▀▀ ─░█░█─ 　 ─▀▀▀▄▄ ░█─ ─░█── ░█▀▀▀ 　 ─▄─░█ ─▀▀▀▄▄ 
+// ░█──▀█ ░█▄▄▀ ░█▄▄▄ ──▀▄▀─ 　 ░█▄▄▄█ ▄█▄ ─░█── ░█▄▄▄ 　 ░█▄▄█ ░█▄▄▄█
+
+// Site-specific JavaScript for their respective individual needs
+
+// ! Requires ndev-global.js to function correctly ! (https://nouhi.dev/assets/global-scripts/ndev-global.js)
+
+document.getElementById("btn").onclick = function () {
+    location.href = "https://nouhidev.github.io/";
+};
